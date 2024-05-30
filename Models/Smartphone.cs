34ceda58a001ce9d0cf4f2d8d65d6f139d0fb763
João Victor <joaovictorcs@hotmail.com>
2024-05-30 16:38:54 -0300

    Desafio de sistema de celulares concluído com sucesso

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,20 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo {get; set;}
+        protected string Imei {get; set;}
 
-        public Smartphone(string numero)
+        protected int Memoria {get; set;}
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
-
+ 
+      
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Nokia celular = new Nokia(numero : "997994826", modelo :  "modelo1", imei : "1", memoria : 128);
+celular.Ligar();
+celular.ReceberLigacao();
+celular.InstalarAplicativo("GitHub");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone celular2 = new Iphone(numero: "999990101", modelo: "modelo2", imei: "2", memoria : 256);
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Linkedin");
